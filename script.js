f6f7829c5162221a1eb756f313fb213721fcde0d
molodoy1911 <komandin-a@bk.ru>
2024-08-20 13:28:05 +0300

    make_API_with_Promise

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-    {}
-  ];
-  
+function makeDETRequest(url) {
+  return new Promise((resolve, reject) => {
+    var xhr;
+
+    if (window.XMLHttpRequest) {
+      xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject(`Error: ${xhr.status}`);
+        }
+      } 
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send();
+  });
+}
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class GoodsItem {
-  constructor(title = 'nothing', price = 0) {
-    this.title = title;
+  constructor(product_name = 'nothing', price = 0) {
+    this.product_name = product_name;
     this.price = price;
   }
   render() {
     return `
       <div class="goods-item">
-        <h3>${this.title}</h3>
+        <h3>${this.product_name}</h3>
         <p>${this.price}</p>
       </div>
     `;
@@ -27,13 +46,19 @@ class GoodsList {
   }
 
   fetchGoods() {
-    this.goods = goods;
+    return makeDETRequest(`${API_URL}/catalogData.json`)
+      .then((goods) => {
+        this.goods = JSON.parse(goods);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
     let listHtml = '';
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
+      const goodItem = new GoodsItem(good.product_name, good.price);
       listHtml += goodItem.render();
     });
     document.querySelector('.goods-list').innerHTML = listHtml;
@@ -43,12 +68,13 @@ class GoodsList {
     let sum = 0;
     this.goods.forEach(good => {
       sum += good.price;
-      console.log(sum);
     });
+    console.log(`Total sum: ${sum}`);
   }
 }
 
 const list = new GoodsList();
-list.fetchGoods();
-list.render();
-list.sum();
+list.fetchGoods().then(() => {
+  list.render();
+  list.sum();
+});
